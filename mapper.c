20a3aa06f9053989e2b13cfe4e89f05f961f40ba
Cedric Dubois <14007887@e212m07.istic.univ-rennes1.fr>
2015-10-21 16:37:05 +0200

    Modification du mapper

--- mapper.c
+++ mapper.c
@@ -9,6 +9,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include "iobank.h"
+#include "immintrin.h"
+#include "nmmintrin.h"
 
 int key (char *s, int j)
 {
@@ -23,7 +25,7 @@ int key (char *s, int j)
   return k;
 }
 
-int __attribute__ ((noinline)) distance (char *reads, char *genome, int k)
+/*int __attribute__ ((noinline)) distance (char *reads, char *genome, int k)
 {
   int d=0;
   int i;
@@ -32,6 +34,77 @@ int __attribute__ ((noinline)) distance (char *reads, char *genome, int k)
       if (reads[i]!=genome[i+k]) d++;
     }
   return d;
+}*/
+
+int __attribute__ ((noinline)) distance (char *reads, char *genome, int k)
+{
+  int d=0;
+  long int r0, r1, r2;
+  long int g0, g1, g2;
+  int i;
+  for (i=0; i<64; i++)
+    {
+      r0 = r0<<2;
+      r0 = r0+((reads[i]>>1)&3);
+      r1 = r1<<2;
+      r1 = r1+((reads[i+64]>>1)&3);
+      r2 = r2<<2;
+      r2 = r2+((reads[i+128]>>1)&3);
+
+      g0 = g0<<2;
+      g0 = g0+((genome[i]>>1)&3);
+      g1 = g1<<2;
+      g1 = g1+((genome[i+64]>>1)&3);
+      g2 = g2<<2;
+      g2 = g2+((genome[i+128]>>1)&3);
+    }
+  
+  // XOR
+/*
+  long int s00 = _mm_xor_si64(r0, g0);
+  long int s01 = _mm_xor_si64(r1, g1);
+  long int s02 = _mm_xor_si64(r2, g2);
+
+  // Séléction des chaines de gauche et de droite par des masques
+  long int s10 = _mm_and_si64(2863311530, s00);
+  long int s20 = _mm_and_si64(1431655765, s00);
+  long int s11 = _mm_and_si64(2863311530, s01);
+  long int s21 = _mm_and_si64(1431655765, s01);
+  long int s12 = _mm_and_si64(2863311530, s02);
+  long int s22 = _mm_and_si64(1431655765, s02);
+
+  // Décalage à gauche
+  s10 = _m_pslldi(s10, 1);
+  s11 = _m_pslldi(s10, 1);
+  s12 = _m_pslldi(s10, 1);
+
+  // OR
+  s10 = _mm_or_si64(s10, s20);
+  s11 = _mm_or_si64(s11, s21);
+  s12 = _mm_or_si64(s12, s22);
+*/
+
+	long int s00 =r0^g0;
+	long int s10 = 2863311530 & s00;
+	long int s20 = 1431655765 & s00;
+	s10 = s10>>1;
+	s10 = s10|s20;
+
+	long int s01 =r1^g1;
+	long int s11 = 2863311530 & s01;
+	long int s21 = 1431655765 & s01;
+	s11 = s11>>1;
+	s11 = s11|s21;
+
+	long int s02 =r2^g2;
+	long int s12 = 2863311530 & s02;
+	long int s22 = 1431655765 & s02;
+	s12 = s12>>1;
+	s12 = s12|s22;
+
+	d = _popcnt64(s10) + _popcnt64(s11) + _popcnt64(s12);
+
+	return d;
 }
 
 void __attribute__ ((noinline)) process_read(struct s_bank *query, int nq, char *genome, int *IDX1, int*IDX2, int *IDX)
